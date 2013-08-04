26b4aa4f306b3cd77f8df1c9c917c75e8e51427c
Jon Schlinkert <dev@sellside.com>
2013-08-04 19:25:17 -0400

    merge lib file into task

--- tasks/replace.js
+++ tasks/replace.js
@@ -1,6 +1,7 @@
 /*
- * grunt-replace
- * https://github.com/erickrdch/grunt-replace
+ * grunt-refactor
+ * Based on grunt-string-replace
+ * https://github.com/erickrdch/grunt-string-replace
  *
  * Copyright (c) 2012 Erick Ruiz de Chavez
  * Licensed under the MIT license.
@@ -9,21 +10,64 @@
 module.exports = function(grunt) {
   'use strict';
 
-  // if grunt is not provided, then expose internal API
-  if ('object' !== typeof(grunt)) {
-    return require('./lib/replace').init(require('grunt'));
-  }
+  var path = require('path');
 
-  var string_replace = require('./lib/replace').init(grunt);
-
-  grunt.registerMultiTask('replace', 'String Replace Task.', function() {
-    var replacements;
-
+  grunt.registerMultiTask('refactor', 'String Replace Task.', function() {
     var options = this.options({
-      replacements: []
+      replacements: [],
+      rename: []
     });
 
-    replacements = string_replace.normalize_replacements(options.replacements);
-    string_replace.replace(this.files, replacements);
+    // Execute replacements
+    var replacements = normalizeReplacements(options.replacements);
+    replace(this.files, replacements);
   });
+
+  // Detect file or directory
+  function detectDestType(dest) {
+    if (grunt.util._.endsWith(dest, '/')) {
+      return 'directory';
+    } else {
+      return 'file';
+    }
+  }
+  // Convert paths to slashes
+  function normalizePath(filepath) {
+    if (process.platform === 'win32') {
+      return filepath.replace(/\\/g, '/');
+    } else {
+      return filepath;
+    }
+  }
+  function replace(files, replacements) {
+    var dest;
+    var isExpandedPair;
+    var content;
+
+    files.forEach(function(filePair) {
+      filePair.src.forEach(function(src) {
+        isExpandedPair = filePair.orig.expand || false;
+
+        if (detectDestType(filePair.dest) === 'directory') {
+          dest = (isExpandedPair) ? filePair.dest : normalizePath(path.join(filePair.dest, src));
+        } else {
+          dest = filePair.dest;
+        }
+
+        content = grunt.file.read(src);
+        content = replaceMultiStrings(content, replacements);
+        grunt.file.write(dest, content);
+      });
+    });
+  }
+  function replaceMultiStrings(string, replacements) {
+    return replacements.reduce(function(content, replacement) {
+      return content.replace(replacement[0], replacement[1]);
+    }, string);
+  }
+  function normalizeReplacements(replacements) {
+    return replacements.map(function(replacement) {
+      return [replacement.pattern, replacement.replacement];
+    });
+  }
 };
